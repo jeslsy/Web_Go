eab6600bab88302ffd8bc923167277e2ffad06cc
jeslsy0507 <jeslsy0507@naver.com>
2021-03-22 20:34:59 +0900

    hello world

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("web with go")
+	// handler 로직을 작성
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	//3000포트로 localhost:3000을 호출
+	http.ListenAndServe(":3000", nil)
 }
