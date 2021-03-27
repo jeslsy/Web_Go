8b63d025e37d66dc61122cf69753d072b433d7b2
jeslsy0507 <jeslsy0507@naver.com>
2021-03-27 19:19:13 +0900

    checking indexHandler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Tucker_Programming/Web_with_Go
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
--- myapp/app_test.go
+++ myapp/app_test.go
@@ -2,12 +2,17 @@
 package myapp
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIndexPathHandler(t *testing.T) {
+	assert := assert.New(t)
+
 	// 네트워크 사용 않고 그냥 사용할 수 있는 레코더
 	res := httptest.NewRecorder()
 	// 새 호출
@@ -15,10 +20,17 @@ func TestIndexPathHandler(t *testing.T) {
 
 	indexHandler(res, req)
 
-	// 실패하면
-	if res.Code != http.StatusBadRequest {
-		// 프로그램 종료
-		t.Fatal("Failed!! ", res.Code)
-	}
+	// 매번 검사하지 말고
+	//if res.Code != http.StatusBadRequest {
+	//	// 프로그램 종료
+	//	t.Fatal("Failed!! ", res.Code)
+	//}
 
+	// "github.com/stretchr/testify/assert"이 패키지를 이용해 확인
+	assert.Equal(http.StatusOK, res.Code)
+
+	//ioutil 패키지로 버퍼값을 다 읽어 오겠음.
+	data, _ := ioutil.ReadAll(res.Body)
+	// byte array 타입이기 때문에 string으로 바꿔서 비교
+	assert.Equal("Hello World", string(data))
 }
